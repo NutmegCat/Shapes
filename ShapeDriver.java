a08e0dfa8fbe4a86fa65914d9c060790ccfbd151
NutmegCat <aaronprince.anu@student.tdsb.on.ca>
2023-10-25 14:48:37 -0400

    spelling error

--- ShapeDriver.java
+++ ShapeDriver.java
@@ -3,8 +3,8 @@ public class ShapeDriver {
     Rectangle r = new Rectangle(3, 5);
     Triangle t = new Triangle(3, 4, 5);
     Square s = new Square(8);
-    System.out.println("r area is " + r.area() + ", r parameter is " + r.perimeter());
-    System.out.println("t area is " + t.area() + ", t parameter is " + t.perimeter());
-    System.out.println("s area is " + s.area() + ", s parameter is " + s.perimeter());
+    System.out.println("r area is " + r.area() + ", r perimeter is " + r.perimeter());
+    System.out.println("t area is " + t.area() + ", t perimeter is " + t.perimeter());
+    System.out.println("s area is " + s.area() + ", s perimeter is " + s.perimeter());
   }
 }
